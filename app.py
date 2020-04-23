32016566a4bbe4a10f7845dfef583ee3e13ed7e2
Burak Tilek <burak.tilek@gmail.com>
2020-04-23 11:26:00 +0300

    Added auth decorators to routes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from models import setup_db, db_create_all, Movie, Actor
+from auth import AuthError, requires_auth
 
 
 def create_app(test_config=None):
@@ -14,7 +15,8 @@ def create_app(test_config=None):
 
     # Movie Endpoints #
     @app.route('/movies', methods=['GET'])
-    def get_movies():
+    @requires_auth('get:movies')
+    def get_movies(payload):
         movies = Movie.query.order_by(Movie.id).all()
 
         formatted_movies = [movie.format() for movie in movies]
@@ -25,7 +27,8 @@ def create_app(test_config=None):
         }), 200
 
     @app.route('/movies/<int:movie_id>', methods=['GET'])
-    def get_movie(movie_id):
+    @requires_auth('get:movie')
+    def get_movie(payload, movie_id):
         movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
 
         try:
@@ -41,7 +44,8 @@ def create_app(test_config=None):
             abort(422)
 
     @app.route('/movies', methods=['POST'])
-    def new_movie():
+    @requires_auth('post:movies')
+    def new_movie(payload):
         body = request.get_json()
 
         try:
@@ -66,7 +70,8 @@ def create_app(test_config=None):
             abort(422)
 
     @app.route('/movies/<int:movie_id>', methods=['PATCH'])
-    def update_movie(movie_id):
+    @requires_auth('patch:movies')
+    def update_movie(payload, movie_id):
         if movie_id is None:
             abort(404)
 
@@ -87,7 +92,8 @@ def create_app(test_config=None):
             abort(404)
 
     @app.route('/movies/<int:movie_id>', methods=['DELETE'])
-    def delete_movie(movie_id):
+    @requires_auth('delete:movies')
+    def delete_movie(payload, movie_id):
         movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
 
         try:
@@ -106,7 +112,8 @@ def create_app(test_config=None):
 
     # Actor Endpoints #
     @app.route('/actors', methods=['GET'])
-    def get_actors():
+    @requires_auth('get:actors')
+    def get_actors(payload):
         actors = Actor.query.order_by(Actor.id).all()
 
         formatted_actors = [actor.format() for actor in actors]
@@ -117,7 +124,8 @@ def create_app(test_config=None):
         }), 200
 
     @app.route('/actors/<int:actor_id>', methods=['GET'])
-    def get_actor(actor_id):
+    @requires_auth('get:actor')
+    def get_actor(payload, actor_id):
         actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
 
         try:
@@ -133,7 +141,8 @@ def create_app(test_config=None):
             abort(422)
 
     @app.route('/actors', methods=['POST'])
-    def new_actor():
+    @requires_auth('post:actors')
+    def new_actor(payload):
         body = request.get_json()
 
         try:
@@ -161,7 +170,8 @@ def create_app(test_config=None):
             abort(422)
 
     @app.route('/actors/<int:actor_id>', methods=['PATCH'])
-    def update_actor(actor_id):
+    @requires_auth('patch:actors')
+    def update_actor(payload, actor_id):
         if actor_id is None:
             abort(404)
 
@@ -183,7 +193,8 @@ def create_app(test_config=None):
             abort(404)
 
     @app.route('/actors/<int:actor_id>', methods=['DELETE'])
-    def delete_actor(actor_id):
+    @requires_auth('delete:actors')
+    def delete_actor(payload, actor_id):
         actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
 
         try:
@@ -246,4 +257,4 @@ def create_app(test_config=None):
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run()
